33182b133b0a1ba1643858331022b360bd80a140
Sofia Alere <81483266+sofalere@users.noreply.github.com>
2023-09-12 17:15:11 -0500

    first app routes

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const port = 3000;
 
+const app = express();
+//const rdb = require('./pg-query');
+
+// allows us to read request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// homepage that displays all bins
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  const bins = { bin0: 'http://ourappname.com/endpoint1', 
+                 bin1: 'http://ourappname.com/endpoint2'}; // eventually enable rdb.getBins()
+  res.send(bins);
+});
+
+// sends the relevant request data from any request made to 
+// `endpoint` (which will be dynamic eventually) using any method
+app.all('/listen/endpoint', (req, res) => {
+  const endpoint = 'endpoint'; // when dynamic will retrieve req.params['endpoint']
+  console.log(endpoint, 'METHOD:', req.method, 'PATH:', req.path, 'BODY:', req.body);
+  // rdb.addRequest(endpoint, req.path, req.method, req.body);
+  res.send(`Any requests sent here are goin in the database! Look at all this data: ENDPOINT: ${endpoint}, METHOD: ${req.method}, PATH: ${req.path}, BODY: ${JSON.stringify(req.body)}`);
+});
+
+// retrieves and displays all the requests in a specific bin
+app.get('/view/:endpoint', (req, res) => {
+  const endpoint = 'endpoint'; // when dynamic will retrieve req.params['endpoint']
+  res.send('Look at alllll these requests for this bin!');
+  // rdb.getRequests(endpoint);
+});
+
+// invalid route handling
+app.get('*', (req, res) => {
+  res.send('Not even a route my guy');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
